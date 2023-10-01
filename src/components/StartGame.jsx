c3c129790eae9883c2df4ec1934df8feda8c0b49
surajaribenchi <surajaribenchi@gmail.com>
2023-10-01 20:16:50 +0530

    second commit

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -2,8 +2,12 @@
 
 function StartGame() {
   return (
-    <div>
-      iyfuyfiylf
+    <div className="max-w-6xl flex items-center" style={{margin:"0 auto",height:"100vh"}}>
+       <div><img src="/images/dices.png" alt="" /></div>
+       <div className="">
+        <h1 className="font-extrabold text-6xl">Dice Game</h1>
+        <button style={{padding:"10px 18px",maxWidth:"220px"}}className="bg-black text-white rounded flex items-center justify-center flex-col hover:bg-sky-700">Play Now</button>
+       </div>
     </div>
   )
 }
